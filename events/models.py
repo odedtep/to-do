b26b1d4666105bbb9a774f28ed3863b5f72c0123
Eneli Verk <eneli.nommik@gmail.com>
2024-08-11 09:23:45 +0300

    Update database

--- events/models.py
+++ events/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
@@ -6,28 +8,32 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
-class UserProfile(models.Model):
-    first_name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=100)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.first_name
 
-class Event_catagory(models.Model):
+# class UserProfile(models.Model):
+#     first_name = models.CharField(max_length=100)
+#     surname = models.CharField(max_length=100)
+#     email = models.EmailField()
+#     phone = models.CharField(max_length=100)
+#     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.first_name
+
+class EventCategory(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
         return self.name
+
+
 class Event(models.Model):
-    category = models.ForeignKey(Event_catagory, on_delete=models.CASCADE)
+    category = models.ForeignKey(EventCategory, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    creator = models.ForeignKey(UserProfile , on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()  # can be optional
     start_time = models.TimeField()
@@ -36,15 +42,15 @@ class Event(models.Model):
     # need to add: pic/video
     # public or private
 
-
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class CartItem(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     event = models.ForeignKey(Event, on_delete=models.CASCADE, blank=True, null=True)
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.title
