6f7a0026531bd49282160ea9076514864c96f0e8
Eneli Verk <eneli.nommik@gmail.com>
2024-08-17 19:53:51 +0300

    final update to weather widget

--- to_do/settings.py
+++ to_do/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-# TICKETMASTER_API_KEY = config('TICKETMASTER_API_KEY')
+TICKETMASTER_API_KEY = config('TICKETMASTER_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
