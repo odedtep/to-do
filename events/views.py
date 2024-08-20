91113a909e8cda27f5ba824af7aaac6fdd896e6c
Maiken Masing <maikenmasing@gmail.com>
2024-08-20 20:38:37 +0300

    ticketmaster api added

--- events/views.py
+++ events/views.py
@@ -6,11 +6,10 @@ from .forms import EventForm
 from django.conf import settings
 from django.http import JsonResponse
 from django.contrib import messages
-from .weather_context import weather_context
+from datetime import datetime
+
 
 def index(request):
-    if 'weather_location' in request.session:
-        del request.session['weather_location']
     locations = Location.objects.all()
     return render(request, 'index.html', {'locations': locations})
 
@@ -25,32 +24,36 @@ def location_detail(request, location_id):
     )
 
 
+# TODO: change the function name to all_events
 def all_events(request):
     location_id = request.GET.get('location')
     date = request.GET.get('date')
-
-    if location_id:
-        weather_location = Location.objects.filter(id=location_id).first()
-        if weather_location:
-            weather_location = weather_location.name
-            request.session['weather_location'] = weather_location
-        else:
-            weather_location = 'Tallinn'
-    else:
-        weather_location = request.session.get('weather_location', 'Tallinn')
-
+    start_date_iso8601 = ''
+    end_date_iso8601 = ''
     events = Event.objects.all()
-
     if location_id:
-        events = events.filter(location__name=weather_location)
-
+        events = events.filter(location_id=location_id)
     if date:
         events = events.filter(start_date=date)
+        start_date_iso8601 = f"{date}T00:00:00Z"
+        end_date_iso8601 = f"{date}T23:59:59Z"
+    city = get_city(location_id)
+    filtered_events = get_ticketmaster_events(request, city, start_date_iso8601, end_date_iso8601)
+    return render(request, 'events.html', {'events': events, 'ticketmaster_events': filtered_events})
 
-    weather = weather_context(request, weather_location)
 
-    return render(request, 'events.html', {'events': events, **weather})
-
+def get_city(location_id):
+    if location_id is None:
+        return ''
+    try:
+        location_id = int(location_id)
+    except ValueError:
+        return ''  # or handle the error accordingly
+    locations = Location.objects.all()
+    for location in locations:
+        if location.id == location_id:
+            city = location.name
+    return city
 
 
 @login_required
@@ -63,34 +66,26 @@ def create_event(request):
             event.save()
             form.save_m2m()
             messages.success(request, 'Your event has been created!')
-            return redirect('all_events')
+            return redirect('events')  ## NEED TO BE CHANGED
     form = EventForm()
     return render(request, 'create_event.html', {'form': form})
 
 
+# need to add a msg if event created successfully
+# TODO: look over lines 71 and 72
 # @login_required
 def event_detail(request, event_id):
     event = get_object_or_404(Event, id=event_id)
-    location_name = event.location.name \
-        if event.location \
-        else 'Tallinn'
-
-    if event.location:
-        request.session['weather_location'] = event.location.name
-
-    weather = weather_context(request, location_name)
-
-    participants = event.participants.count()
     if request.method == 'POST' and request.user.is_authenticated:
         if event.payment_type == Event.PAY_TO_JOIN and event.payment_amount > 0:
             CartItem.objects.get_or_create(event=event, user=request.user)
             return redirect('user_cart')
         elif event.payment_type == Event.PAY_FOR_TASK and request.user == event.creator:
             return redirect('event_detail', event_id=event.id)
-    return render(request, 'event_detail.html',
-                  {'event': event,
-                   'participants': participants,
-                   **weather})
+        else:
+            CartItem.objects.get_or_create(event=event, user=request.user)
+            return redirect('user_cart')
+    return render(request, 'event_detail.html', {'event': event})
 
 
 @login_required
@@ -102,8 +97,6 @@ def event_view(request, event_id):
 @login_required
 def add_to_cart(request, event_id):
     event = get_object_or_404(Event, id=event_id)
-    if not request.user in event.participants.all():
-        event.participants.add(request.user)
     CartItem.objects.create(event=event, user=request.user)
     return redirect('user_cart')
 
@@ -114,19 +107,24 @@ def user_cart(request):
     return render(request, 'user_cart.html', {'cart_items': cart_items})
 
 
-def get_ticketmaster_events(request, city):
+def get_ticketmaster_events(request, city, start_date_iso8601, end_date_iso8601):
     url = 'https://app.ticketmaster.com/discovery/v2/events.json'
     params = {
         'apikey': settings.TICKETMASTER_API_KEY,
         'city': city,
-        'size': 10
+        'startDateTime': start_date_iso8601,
+        'endDateTime': end_date_iso8601,
+        'size': 100,
     }
-    response = requests.get(url, params=params)
-
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an exception for 4xx/5xx errors
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({'error': str(e)}, status=500)
+    # response = requests.get(url, params=params)
     if response.status_code == 200:
         data = response.json()
         events_data = data.get('_embedded', {}).get('events', [])
-
         filtered_events = []
         for event in events_data:
             # Extract the required fields
@@ -141,21 +139,22 @@ def get_ticketmaster_events(request, city):
             address_line1 = venue_info.get('address', {}).get('line1')
             longitude = venue_info.get('location', {}).get('longitude')
             latitude = venue_info.get('location', {}).get('latitude')
-
+            event_url = event.get('url')
             # Create a new event dictionary
             filtered_event = {
-                'name': event_name,
+                'title': event_name,
                 'image_url': third_image_url,
                 'start_date': start_date,
                 'start_time': start_time,
-                'city_name': city_name,
+                'location': city_name,
                 'address_line1': address_line1,
                 'longitude': longitude,
                 'latitude': latitude,
-                'venue': venue_name
+                'venue': venue_name,
+                'url': event_url
             }
+            # print(start_date)
             filtered_events.append(filtered_event)
-
-        return JsonResponse(filtered_events, safe=False)
+        return filtered_events
     else:
         return JsonResponse({'error': 'Failed to fetch data from Ticketmaster API'}, status=response.status_code)
