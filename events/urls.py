86ba76ca7ae0e8e92e0c537c2e57cdd11f0508db
Eneli Verk <eneli.nommik@gmail.com>
2024-08-23 11:46:29 +0300

    video added to landing page

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import delete_event_from_cart
 from . import views
 
 urlpatterns = [
@@ -11,6 +11,9 @@ urlpatterns = [
     path('event/<int:event_id>/view/', views.event_view, name='event_view'),
     path('event/<int:event_id>/add_to_cart/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.user_cart, name='user_cart'),
+    path('event/<int:event_id>/delete/', delete_event_from_cart, name='delete_event_from_cart'),
+    path('event/delete/<int:event_id>/', views.delete_event, name='delete_event'),
+
     # path('ticketmaster/<str:city>/', views.get_ticketmaster_events, name='ticketmaster-city')
     # 21.08 maiken:
     # path('ticketmaster_event/<str:event_id>/', views.ticketmaster_event_detail, name='ticketmaster_event_detail'),
